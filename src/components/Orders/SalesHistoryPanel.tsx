31df1e7bcefbb2187ded8fb7f67f71245ba0f0b0
gleilsonbarbosa <eliteacaimkt@gmail.com>
2025-08-22 15:54:40 -0300

    Updated SalesHistoryPanel.tsx

--- src/components/Orders/SalesHistoryPanel.tsx
+++ src/components/Orders/SalesHistoryPanel.tsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import SalePrintView from '../PDV/SalePrintView';
+import SalePrintView from '/PDV/SalePrintView';
 import SalePrintView from '../PDV/SalePrintView';
 import SalePrintView from '../PDV/SalePrintView';
 import SalePrintView from '../PDV/SalePrintView';
