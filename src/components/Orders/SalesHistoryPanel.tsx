9282df2bfc94937eb5201448b2b3cab6c270bb34
gleilsonbarbosa <eliteacaimkt@gmail.com>
2025-08-24 15:51:12 -0300

    Updated SalesHistoryPanel.tsx

--- src/components/Orders/SalesHistoryPanel.tsx
+++ src/components/Orders/SalesHistoryPanel.tsx
@@ -18,7 +18,7 @@ import CashRegisterMenu from '../PDV/CashRegisterMenu';
 import SalesHistoryPanel from '../Orders/SalesHistoryPanel';
 import TableSalesPanel from '../TableSales/TableSalesPanel';
 import { usePermissions } from '../../hooks/usePermissions';
-import { useScale } from '../hooks/useScale';
+import { useScale } from '../../hooks/useScale';
 import { useOrders } from '../hooks/useOrders';
 import { usePDVCashRegister } from '../hooks/usePDVCashRegister';
 import { useStoreHours } from '../hooks/useStoreHours';
