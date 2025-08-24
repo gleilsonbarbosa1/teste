90baf50bdf3dbb3911b01e2c0ccbeb9b61462ed1
gleilsonbarbosa <eliteacaimkt@gmail.com>
2025-08-24 15:39:12 -0300

    Updated SalesHistoryPanel.tsx

--- src/components/Orders/SalesHistoryPanel.tsx
+++ src/components/Orders/SalesHistoryPanel.tsx
@@ -17,7 +17,12 @@ import PDVSalesScreen from '../PDV/PDVSalesScreen';
 import CashRegisterMenu from '../PDV/CashRegisterMenu';
 import SalesHistoryPanel from '../Orders/SalesHistoryPanel';
 import TableSalesPanel from '../TableSales/TableSalesPanel';
-
+import { usePermissions } from '/hooks/usePermissions';
+import { useScale } from '../hooks/useScale';
+import { useOrders } from '../hooks/useOrders';
+import { usePDVCashRegister } from '../hooks/usePDVCashRegister';
+import { useStoreHours } from '../hooks/useStoreHours';
+import { PDVOperator } from '../types/pdv'; 
 
 interface UnifiedAttendancePanelProps {
   operator?: PDVOperator;
