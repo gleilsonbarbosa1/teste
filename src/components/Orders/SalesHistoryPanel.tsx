92f2fd11e0f9797a0ccb8ea9bf863d3d32620eb4
gleilsonbarbosa <eliteacaimkt@gmail.com>
2025-08-24 15:38:02 -0300

    Updated SalesHistoryPanel.tsx

--- src/components/Orders/SalesHistoryPanel.tsx
+++ src/components/Orders/SalesHistoryPanel.tsx
@@ -12,11 +12,11 @@ import {
   LogOut,
   Users
 } from 'lucide-react';
-import AttendantPanel from './Orders/AttendantPanel'; 
-import PDVSalesScreen from './PDV/PDVSalesScreen';
-import CashRegisterMenu from './PDV/CashRegisterMenu';
-import SalesHistoryPanel from './Orders/SalesHistoryPanel';
-import TableSalesPanel from './TableSales/TableSalesPanel';
+import AttendantPanel from '../Orders/AttendantPanel'; 
+import PDVSalesScreen from '../PDV/PDVSalesScreen';
+import CashRegisterMenu from '../PDV/CashRegisterMenu';
+import SalesHistoryPanel from '../Orders/SalesHistoryPanel';
+import TableSalesPanel from '../TableSales/TableSalesPanel';
 import { usePermissions } from '../hooks/usePermissions';
 import { useScale } from '../hooks/useScale';
 import { useOrders } from '../hooks/useOrders';
